56c4db725c5655c1bfff9c1ab5d48b5290a26d28
forhad-eng <forhadu.cse@gmail.com>
2022-04-29 19:26:05 +0600

    database setup and tested

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 const express = require('express')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb')
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9bqnm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })
+
+async function run() {
+    try {
+        await client.connect()
+        const inventoryCollection = client.db('carInventory').collection('car')
+
+        app.get('/car', async (req, res) => {
+            const cursor = inventoryCollection.find({})
+            const cars = await cursor.toArray()
+            res.send(cars)
+        })
+    } finally {
+    }
+}
+
+run().catch(console.dir)
 
 app.use(cors())
 app.use(express.json())
