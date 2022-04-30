9e9df3f0a2b422fce395418a9c1584d785433792
forhad-eng <forhadu.cse@gmail.com>
2022-04-30 14:57:37 +0600

    single inventory item deletion api created

--- index.js
+++ index.js
@@ -35,6 +35,13 @@ async function run() {
             const result = await inventoryCollection.updateOne(filter, updatedDoc, options)
             res.send(result)
         })
+
+        app.delete('/car/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const result = await inventoryCollection.deleteOne(query)
+            res.send(result)
+        })
     } finally {
     }
 }
